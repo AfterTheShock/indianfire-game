f25f0963a126dd8dbf36bf53c3b69697506022b7
GonzaloBlanco <117306826+GonzalitoL@users.noreply.github.com>
2025-08-30 16:16:07 -0300

    Movimiento del pj con pot

---
+++ Assets/Scripts/MovementWithPot.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+
+public class MovementWithPot : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 25f;
+    [SerializeField] float rotationSpeed = 25f;
+    [SerializeField] float velocityToStopRotation = 15f;
+    [SerializeField] Vector2 rotationLimits = new Vector2 (45, 315);
+    [SerializeField] float velToStartRotating = 15f;
+    [SerializeField] float rotationDificultySpeed = 15f;
+    [SerializeField] Transform rotationTransform;
+    [SerializeField] Animator animator;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float playerHeight = 3f;
+    [SerializeField] float RotationToSlopSpeed = 2f;
+
+    private InputSystem_Actions inputs;
+
+    private float arrowsInputs;
+    private float wasdInputs;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        inputs = new InputSystem_Actions();
+        inputs.Player.Enable();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        RotationManager();
+        GetPlayerInputs();
+        RotatePlayerBySpeed();
+        ManagePlayerAnimations();
+        //ManageWorldRotationRelativeToTheGround();
+    }
+
+    private void ManageWorldRotationRelativeToTheGround()
+    {
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(this.transform.position, -this.transform.up, playerHeight, groundMask);
+        if (Physics2D.Raycast(this.transform.position,-this.transform.up, playerHeight, groundMask))
+        {
+            float angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg;
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.AngleAxis(angle -90, Vector3.forward), RotationToSlopSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ManagePlayerAnimations()
+    {
+        if (wasdInputs != 0) animator.Play("WalkWithPotPlayer");
+        if(wasdInputs > 0) rotationTransform.localScale = new Vector3(1,1,1);
+        if(wasdInputs < 0) rotationTransform.localScale = new Vector3(-1,1,1);
+    }
+
+    private void GetPlayerInputs()
+    {
+        if (inputs.Player.Arrows.ReadValue<Vector2>().x != 0) arrowsInputs = inputs.Player.Arrows.ReadValue<Vector2>().x;
+        else arrowsInputs = Mathf.Lerp(arrowsInputs, 0, velocityToStopRotation * Time.deltaTime);
+
+        wasdInputs = inputs.Player.Move.ReadValue<Vector2>().x;
+    }
+    private void RotatePlayerBySpeed()
+    {
+        float rotationDificulty = rotationDificultySpeed;
+
+        if (arrowsInputs != 0) rotationDificulty = rotationDificultySpeed / 2;
+
+        if (rb.linearVelocity.x > velToStartRotating)
+        {
+            TryToRotatePlayer(rotationDificulty, 1 * Mathf.Abs(rb.linearVelocity.x));
+        }
+        if (rb.linearVelocity.x < -velToStartRotating)
+        {
+            TryToRotatePlayer(rotationDificulty, -1 * Mathf.Abs(rb.linearVelocity.x));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ManagePlayerMovement ();
+    }
+    private void ManagePlayerMovement()
+    {
+        if(wasdInputs != 0) rb.linearVelocity = new Vector2(wasdInputs * movementSpeed * Time.fixedDeltaTime, rb.linearVelocity.y);
+    }
+
+    private void RotationManager()
+    {
+
+        TryToRotatePlayer(rotationSpeed, arrowsInputs);
+    }
+
+    private void TryToRotatePlayer(float rotationSpeed, float input)
+    {
+        Vector3 oldAngles = rotationTransform.localEulerAngles;
+
+        rotationTransform.localEulerAngles += (new Vector3(0, 0, -input * rotationSpeed * Time.deltaTime));
+
+        if (rotationTransform.localEulerAngles.z < rotationLimits.y && rotationTransform.localEulerAngles.z > rotationLimits.x)
+        {
+            rotationTransform.localEulerAngles = oldAngles;
+            FallOverWithPot();
+        }
+    }
+
+    private void FallOverWithPot()
+    {
+        Debug.Log("No esta implementado el caerse con el jarron");
+    }
+}
